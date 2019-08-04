41134a3d8c4f4e9deee249e8e5159ec11f4eec3c
Fabio Codiglioni <fabiocody@icloud.com>
2019-08-04 18:27:57 +0200

    Implement neighbor discovery in bins

--- src/messages.h
+++ src/messages.h
@@ -1,8 +1,23 @@
 // messages.h
 
+#include "contiki.h"
+
 
 // IDEAS
 // msg_id
+
+
+#define ANNOUNCE_MSG  0
+#define    ALERT_MSG  1
+#define    TRUCK_MSG  2
+#define     MOVE_MSG  3
+#define    TRASH_MSG  4
+#define   MOVE_REPLY  5
+
+
+typedef struct {
+    unsigned char type;
+} announce_msg_t;
 
 
 typedef struct {
@@ -36,8 +51,4 @@ typedef struct {
 } trash_msg_t;
 
 
-#define ALERT_MSG   1
-#define TRUCK_MSG   2
-#define MOVE_MSG    3
-#define MOVE_REPLY  4
-#define TRASH_MSG   5
+#define GET_MSG_TYPE(msg) (*(unsigned char *)msg)
