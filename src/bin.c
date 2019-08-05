544bd7235dacaa45af572249e28e7ea4481587b5
Alessandro Nichelini <ale.nichelg@gmail.com>
2019-08-05 15:34:04 +0200

    added trasg generation process implementation and full bin mode skeleton

--- src/bin.c
+++ src/bin.c
@@ -16,13 +16,21 @@ typedef struct neighbor {
 
 
 #define MAX_NEIGHBORS 8
+#define ALERT_EVENT 254
+#define FULL_EVENT 255
+
+
 MEMB(neighbors_memb, neighbor_t, MAX_NEIGHBORS);
 LIST(neighbors_list);
 
 
 PROCESS(announce_proc, "Announce process");
 PROCESS(debug_print_proc, "DEBUG Print process");
-AUTOSTART_PROCESSES(&announce_proc, &debug_print_proc);
+PROCESS(trash_proc, "Trash generation process");
+PROCESS(alert_mode_proc, "Alert mode process");
+PROCESS(full_mode_proc, "Full bin mode process");
+
+AUTOSTART_PROCESSES(&announce_proc, &debug_print_proc, &trash_proc, &alert_mode_proc, full_mode_proc);
 
 
 static void handle_announce_msg(const rimeaddr_t *from) {
@@ -55,8 +63,11 @@ static void broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from) {
 
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
+static unsigned char trash = 0;
+static unsigned char x = 0;
+static unsigned char y= 0;
 
-
+// NEIGHBOUR DISCOVERY PROCESS
 PROCESS_THREAD(announce_proc, ev, data) {
     static struct etimer et;
     static announce_msg_t msg;
@@ -72,7 +83,65 @@ PROCESS_THREAD(announce_proc, ev, data) {
     PROCESS_END();
 }
 
+//
 
+PROCESS_THREAD(trash_proc, ev, data) {
+    static struct etimer et;
+    static unsigned char gen_trash = 0;
+    PROCESS_BEGIN();
+    x = random_rand() % 100; // x coordinate random initialization
+    y = random_rand() % 100; // y coordinate random initiliazation
+    while (1) {
+        etimer_set(&et, CLOCK_SECOND * (1 + random_rand() % 30)); // set timer with random value
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+        gen_trash += 1 + random_rand() % 10; // trash generation
+        
+        if (trash + gen_trash < 100) {
+            trash += gen_trash;
+            if (trash >= 85)
+                process_post(&alert_mode_proc, ALERT_EVENT, NULL);
+        } else {
+            process_post(&full_mode_proc, FULL_EVENT, NULL);
+        }    
+    }
+    PROCESS_END();
+}
+
+PROCESS_THREAD(alert_mode_proc, ev, data) {
+    static struct etimer et;
+    static alert_msg_t msg;
+    PROCESS_BEGIN();
+    while(1) {
+        PROCESS_WAIT_EVENT();
+        if (ev == ALERT_EVENT) 
+            while(1) {
+                etimer_set(&et, CLOCK_SECOND * 5);
+                PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+                msg.type = ALERT_MSG;
+                msg.x = x;
+                msg.y = y;
+                msg.id = rimeaddr_node_addr.u8[0]; // getting id from rime stack
+                packetbuf_copyfrom(&msg, sizeof(alert_msg_t));
+                broadcast_send(&broadcast);
+            }
+    }
+    PROCESS_END();
+}
+
+PROCESS_THREAD(full_mode_proc, ev, data) {
+    static move_msg_t msg;
+    PROCESS_BEGIN();
+    while(1) {
+        PROCESS_WAIT_EVENT();
+        if (ev == FULL_EVENT) { 
+            while(1){
+            }
+        }
+    }
+    PROCESS_END();
+}
+
+// DEBUGGING
 PROCESS_THREAD(debug_print_proc, ev, data) {
     static struct etimer et;
     PROCESS_BEGIN();
